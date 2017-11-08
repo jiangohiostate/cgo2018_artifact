7a4c68857f44f2df3eb577b48c342ec8d1dda66d
Peng Jiang <binren@login3.stampede2.tacc.utexas.edu>
2017-11-08 16:10:06 -0600

    update script

--- get_data.sh
+++ get_data.sh
@@ -1,4 +1,5 @@
 
+echo -e "\e[33m The input files are pulled from dropbox, please make sure you have access to dropbox... \e[0m"
 echo "Getting data for graph applications .... "
 cd graphs
 wget https://www.dropbox.com/s/1guu18eba6hr5ot/datasets.tar?dl=0 -O datasets.tar
@@ -12,6 +13,7 @@ tar xf input.tar
 rm input.tar
 
 echo "Generating data for hash-based aggregation ..... "
+echo -e "\e[33mIt will take about one hour to generate all the data... \e[0m"
 cd ../hash_agg
 cd datasets
 for i in 6 7 8 9 10 11 12 13 14 15 16 17 18 19; do
